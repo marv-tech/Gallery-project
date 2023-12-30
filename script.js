3e564c6b224e40d5a46016e1bde8e116d0ed9bc1
Chukwudi Marvis <marvischukz@gmail.com>
2023-12-30 14:02:47 +0100

    restructuring

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-const randomImage_URL = "https://api.unsplash.com"
-fetch (randomImage_URL, {
-    method: 'get',
-    headers:{
-        Authorization: 'Y7PQHND2WUFyWG6VWU5R2c5YiLUVWDM3bjv4bGDU72k'
-    },
-    mode:'no-cors',
-    cache:'default',
-})
-
-    .then(function (response){
-        return response.json()
-    })
-
-    .then (function (data){
-        console.log(data)
-    })
-        
-
+// const randomImage_URL = "https://api.unsplash.com"
+// fetch (randomImage_URL, {
+//     method: 'get',
+//     headers:{
+//         Authorization: 'Y7PQHND2WUFyWG6VWU5R2c5YiLUVWDM3bjv4bGDU72k'
+//     },
+//     mode:'no-cors',
+//     cache:'default'
+// })
+// .then(res => res.json())
+// .then (data => console.log(data))
+// .catch(error => console.log(error))
     
 
+const images = document.querySelectorAll('img')   
 
+function fixHeight (){
+    var height =Math.floor(Math.random() * (500- 300+1)) + 300;
+    for (let i = 0; i < images.length; i++) {
+        console.log('hi')
+
+        images[i].style.height = height; 
+    }
+    console.log('hi')
+}
+
+fixHeight ();
 
     
