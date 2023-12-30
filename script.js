e73ffa41cba62b87679770cac69f10dd77f8ed51
Chukwudi Marvis <marvischukz@gmail.com>
2023-12-30 17:19:01 +0100

    image modal

--- script.js
+++ script.js
@@ -11,19 +11,57 @@
 // .then (data => console.log(data))
 // .catch(error => console.log(error))
     
+document.addEventListener('DOMContentLoaded', () =>{
+    const images = document.querySelectorAll('img')   
 
-const images = document.querySelectorAll('img')   
+    function fixHeight (){
+        var height =Math.floor(Math.random() * (500- 300+1)) + 300;
+        for (let i = 0; i < images.length; i++) {
 
-function fixHeight (){
-    var height =Math.floor(Math.random() * (500- 300+1)) + 300;
-    for (let i = 0; i < images.length; i++) {
-        console.log('hi')
-
-        images[i].style.height = height; 
+            images[i].style.height = height; 
+        }
     }
-    console.log('hi')
-}
 
-fixHeight ();
+    fixHeight ();
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+
+    function getImage(){
+        var imglist= document.querySelectorAll(".grid-item")
+        imglist = Array.from(imglist)
+        console.log(imglist)
+
+
+        for (let index = 0; index < imglist.length; index++) {
+            const img = imglist[index];
+            
+
+            img.addEventListener('click', function(){
+                modal.style.display = "block";
+                modalImg.src = this.src;
+                captionText.innerHTML = this.alt;
+            }) 
+            
+            
+        }
+        console.log('display this')
+    }
+
+    getImage()
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
 
     
+})
